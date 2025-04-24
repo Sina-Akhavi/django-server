b75fe74583182a5df86ee95af39871a6af5af206
Sina Akhavi <sinaakhavispecial1984@gmail.com>
2025-04-24 20:11:29 +0330

    added a url for the getting user

--- myserver/home/urls.py
+++ myserver/home/urls.py
@@ -1,9 +1,8 @@
 
 from django.urls import path
-from home.views import HomeView
+from home.views import HomeView, GetUser
 
 urlpatterns = [
     path('test/', HomeView.as_view(), name='register'),
+    path('user/', GetUser.as_view(), name='get_user'),
 ]
-
-
