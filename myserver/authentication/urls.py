4d31c4b4e8aca277c525063e13dee7bb043c2c75
Sina Akhavi <sinaakhavispecial1984@gmail.com>
2025-04-24 20:49:24 +0330

    updated urls for view updating

--- myserver/authentication/urls.py
+++ myserver/authentication/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path, include
-from authentication.Views.authentication import RegisterView, LoginView
+from authentication.Views.authentication import RegisterView, LoginView, CustomTokenRefreshView
+from home.views import UpdateUserInfo
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('refresh/token/', CustomTokenRefreshView.as_view(), name='refresh-token'),
 ]
--- myserver/home/urls.py
+++ myserver/home/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from home.views import HomeView, GetUser
+from home.views import HomeView, GetUser, UpdateUserInfo
 
 urlpatterns = [
     path('test/', HomeView.as_view(), name='register'),
     path('user/', GetUser.as_view(), name='get_user'),
+    path('user/update-info/', UpdateUserInfo.as_view(), name='get_user'),
 ]
