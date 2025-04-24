1ad96ab2b0e79e2a2cd2d5f2909feb8dfacd739b
Sina Akhavi <sinaakhavispecial1984@gmail.com>
2025-04-24 20:50:02 +0330

    replace update user info

--- myserver/authentication/Views/authentication.py
+++ myserver/authentication/Views/authentication.py
@@ -54,43 +54,8 @@ class LoginView(APIView):
             "access": access_token,
             "refresh": refresh_token
         })
-
-
-class UpdateUserInfo(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request):
-        try:
-            user = request.user
-            
-            city = request.data.get('city')
-            country = request.data.get('country')
-            first_name = request.data.get('first_name')
-            last_name = request.data.get('last_name')
-            username = request.data.get('username')
-            email = request.data.get('email')
-            
-            if city is not None:
-                user.city = city
-            if country is not None:
-                user.country = country
-            if first_name is not None:
-                user.first_name = first_name
-            if last_name is not None:
-                user.last_name = last_name
-            if username is not None:
-                user.username = username
-            if email is not None:
-                user.email = email
-            
-            user.save()
-            
-            return Response({"detail": "User information updated successfully."}, status=status.HTTP_200_OK)
-        except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)    
         
-        
-        
+          
 class CustomTokenRefreshView(TokenRefreshView):
     permission_classes = [AllowAny]  # Make this accessible for everyone (no authentication needed)
 
@@ -102,9 +67,6 @@ class CustomTokenRefreshView(TokenRefreshView):
 
         try:
             refresh = RefreshToken(refresh_token)
-            
-            print(f"Refresh token received for user: {refresh['user_id']}")
-
             access_token = str(refresh.access_token)
             return Response({"access": access_token}, status=status.HTTP_200_OK)
 
