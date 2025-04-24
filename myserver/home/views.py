94577ae71ba2bdb7342cfdfe5ee01f797059567f
Sina Akhavi <sinaakhavispecial1984@gmail.com>
2025-04-24 20:05:58 +0330

    added a view for getting user

--- myserver/home/views.py
+++ myserver/home/views.py
@@ -1,10 +1,24 @@
 from django.http import HttpResponse
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
-
+from myserver.home.services import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 class HomeView(APIView):
     permission_classes = [IsAuthenticated]  # Ensure the user is authenticated
     
     def get(self, request):
         return HttpResponse('Hi Dear!!')
+
+class GetUser(APIView):
+    permission_classes = [IsAuthenticated]  # Ensure the user is authenticated
+    
+    def get(self, request):
+        try:
+            user = request.user
+            data = UserSerializer(user)  
+            return Response(data.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    
