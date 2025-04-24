e50d6e6e9123580f6b92d96a0043ea4635fe8675
Sina Akhavi <sinaakhavispecial1984@gmail.com>
2025-04-24 23:04:17 +0330

    fixed the error for cors

--- myserver/myserver/settings.py
+++ myserver/myserver/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -62,10 +63,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORSE_ALLOW_ORIGINS = [
+CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
 ]
 
