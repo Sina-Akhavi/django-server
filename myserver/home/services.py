5dfac5bba975fbcc04927178ff0007d2171810c9
Sina Akhavi <sinaakhavispecial1984@gmail.com>
2025-05-01 16:18:15 +0330

    added phone to serilizer

--- myserver/home/services.py
+++ myserver/home/services.py
@@ -5,5 +5,5 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'date_joined', 'city', 'country']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'date_joined', 'city', 'country', 'phone_number']
         
